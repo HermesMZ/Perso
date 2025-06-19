2a8c83b04279836d3bfb995b2df1c8e37b709872
Mathieu Zimeris <mzimeris@f0r7s4.clusters.42paris.fr>
2025-06-19 16:43:56 +0200

    je vais finir par l'avoir

--- push_swap/src/push_swap_sort.c
+++ push_swap/src/push_swap_sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap_sort.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zoum <zoum@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: mzimeris <mzimeris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 23:27:14 by zoum              #+#    #+#             */
-/*   Updated: 2025/06/19 02:11:56 by zoum             ###   ########.fr       */
+/*   Updated: 2025/06/19 16:42:54 by mzimeris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static t_swap_int	*stack_split(t_swap *swap, t_swap_int *pivot,
 {
 	t_swap_int	*last_pushed;
 	t_swap_int	*next;
-	
+
 	if (!*current)
 		return (NULL);
 	next = (*current)->next;
@@ -25,7 +25,7 @@ static t_swap_int	*stack_split(t_swap *swap, t_swap_int *pivot,
 		last_pushed = swap->stack_b->first;
 	else
 		last_pushed = swap->stack_a->first;
-	ft_printf("current index %d pivot %d \n", (*current)->index, pivot->index);
+	// ft_printf("current %d pivot %d \n", (*current)->value, pivot->value);
 	if ((*current)->index >= pivot->index)
 	{
 		ft_push(swap, *current);
@@ -35,46 +35,89 @@ static t_swap_int	*stack_split(t_swap *swap, t_swap_int *pivot,
 	else
 		ft_rotate(swap, (*current));
 	*current = next;
-	debug_print_stacks(swap);
-	
+	// debug_print_stacks(swap);
 	return (last_pushed);
 }
 
-// static void	stack_merge(t_swap *swap, t_elem *elem, int count)
-// {
-	
-// }
+static void	merge_split(t_swap *swap, t_swap_int *first, t_stack *to, int count)
+{
+	int			i;
 
-void	quick_sort_stack(t_swap *swap, t_swap_int *first, int count)
+	ft_printf(" ======================== MERGE ======================== \n");
+	if (!first)
+		return ((void)ft_printf("merge first is NULL\n"));
+	if (!to)
+	{
+		if (first->stack == swap->stack_a)
+			to = swap->stack_b;
+		else
+			to = swap->stack_a;
+	}
+	// ft_printf("first->value %d to->value %d \n", first->value, to_first->value);
+	// debug_print_stacks(swap);
+
+	i = 0;
+	rotate_to(swap, first);
+	rotate_to(swap, to->first);
+	// debug_print_stacks(swap);
+
+	while (i < count)
+	{
+		if (first->index < to->first->index && first->index > to->first->prev->index)
+			ft_push(swap, first);
+		else if (first->index > to->first->index && first->index < to->first->next->index)
+		{
+			ft_rotate(swap, to->first);
+			ft_push(swap, first);
+		}
+		i++;
+	}
+	// debug_print_stacks(swap);
+	ft_printf(" ========================= END ========================= \n");
+}
+
+t_stack	*recursive_split_call(t_swap *swap, t_swap_int *first, int count)
 {
 	t_swap_int	*pivot;
 	t_swap_int	*current;
 	t_swap_int	*last_pushed;
 	int			i;
 	int			push_count;
+	debug_print_stacks(swap);
+	
+	pivot = find_median(first, count);
+	ft_printf(" =================NEW PIVOT = %d - count = %d ================== \n", pivot->value, count);
+	current = first;
+	push_count = 0;
+	last_pushed = NULL;
+	i = 0;
+	while (i < count)
+	{
+		last_pushed = stack_split(swap, pivot, &current, &push_count);
+		i++;
+	}
+	if (push_count > 0)
+	{
+		quick_sort_stack(swap, last_pushed, push_count);
+		return (current->stack);
+	}
+	else if (count - push_count > 0)
+	{
+		quick_sort_stack(swap, current, count - push_count);
+		return (last_pushed->stack);
+	}
+	return (NULL);
+}
 
-	if (!first || count <= 1)
+void	quick_sort_stack(t_swap *swap, t_swap_int *first, int count)
+{
+	t_stack	*to;
+
+	to = NULL;
+	if (!first)
 		return ;
 	rotate_to(swap, first);
-	if (count > 2)
-	{
-		pivot = find_median(first, count);
-		current = first;
-		push_count = 0;
-		last_pushed = NULL;
-		i = 0;
-		while (i < count)
-		{
-			// problème quand rien n'est push : solution temporaire trop de variable
-			last_pushed = stack_split(swap, pivot, &current, &push_count);
-			i++;
-		}
-		if (push_count > 0)
-			quick_sort_stack(swap, last_pushed, push_count);
-		if (count - push_count > 0)
-			quick_sort_stack(swap, current, count - push_count);
-	}
-	else
-		hard_sort(swap, first, count);
-
+	if (count > 1)
+		to = recursive_split_call(swap, first, count);
+	merge_split(swap, first, to, count);
 }

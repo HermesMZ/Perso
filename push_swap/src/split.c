fc231d9d8022bc724ebb274715da831ee4a7f381
Hermes <mathieuhermesfr@gmail.com>
2025-06-27 00:46:49 +0200

    ignore objs

--- push_swap/src/split.c
+++ push_swap/src/split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   split.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mzimeris <mzimeris@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zoum <zoum@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 19:38:53 by zoum              #+#    #+#             */
-/*   Updated: 2025/06/26 22:03:00 by mzimeris         ###   ########.fr       */
+/*   Updated: 2025/06/27 00:45:10 by zoum             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,11 @@ static t_heads	*split_with_pivot(t_swap *swap, t_swap_int *pivot,
 	t_swap_int	*current;
 	int			i;
 
-	if (count <= 0)
-		return (NULL);
 	current = pivot->stack->first;
-	ft_printf("before split\n");
-	debug_print_stacks(swap);
-	debug_print_split(heads);
-	ft_printf("pivot %d\n", pivot->value);
-	ft_printf("closest locked %d\n", find_closest_inf_locked(swap->stack_a, current)->value);
-	if (pivot->stack == swap->stack_b)
-	{
-		ft_printf("pushed head %d\n", find_pushed_head_split(pivot, count)->value);
-		rotate_to(swap, find_closest_inf_locked(swap->stack_a, find_pushed_head_split(pivot, count)));
-		ft_printf("after rotate\n");
-		debug_print_stacks(swap);
-	}
 	i = 0;
 	while (i < count)
 	{
-		if (current->index > pivot->index && !current->locked)
+		if (current->index > pivot->index)
 		{
 			ft_push(swap, current);
 			heads->push_count++;
@@ -44,31 +30,28 @@ static t_heads	*split_with_pivot(t_swap *swap, t_swap_int *pivot,
 		else
 		{
 			ft_rotate(swap, current);
-			if (!current->locked)
-				heads->remaining_count++;
+			heads->remaining_count++;
 		}
 		current = pivot->stack->first;
-		if (!current->locked)
-			i++;
+		i++;
 	}
-	lock_all(swap, heads);
-	ft_printf("after split\n");
-	debug_print_stacks(swap);
-	debug_print_split(heads);
-	if (heads->push_count == 0 && heads->remaining_count <= 3)
-	{
-		ft_printf("************************** anticipated merge ******************** \n");
-		hard_sort(swap, heads->remaining, heads->remaining_count);
-		anticipate_merge(swap, &heads);
-	}
-	head_update(heads);
-
-	ft_printf("after split\n");
-	debug_print_stacks(swap);
-	debug_print_split(heads);
-
 	return (heads);
 }
+
+// static void	hard_sort_stacks(t_swap *swap, t_heads *heads)
+// {
+// 	ft_printf("DEBUT DU SORT\n");
+// 	debug_print_split(heads);
+// 	// debug_print_stacks(swap);
+// 	hard_sort(swap, heads->pushed, heads->push_count);
+// 	ft_printf("rotate\n");
+// 	debug_print_split(heads);
+
+// 	rotate_to(swap, heads->remaining);
+// 	hard_sort(swap, heads->remaining, heads->remaining_count);
+// 	ft_printf("FIN DU SORT\n");
+// 	debug_print_stacks(swap);
+// }
 
 t_swap_int	*recursive_split_call(t_swap *swap, t_swap_int *first, int count)
 {
@@ -76,8 +59,6 @@ t_swap_int	*recursive_split_call(t_swap *swap, t_swap_int *first, int count)
 	t_swap_int	*pivot;
 	t_swap_int	*result;
 
-	if (count <= 0)
-		return (NULL);
 	pivot = find_median(first, count);
 	heads = init_heads(count, pivot);
 	split_with_pivot(swap, pivot, heads, count);

b6f48fc5c82838f4f21d7dc7de5573b1a16de8a8
Hermes <mathieuhermesfr@gmail.com>
2025-07-01 23:16:28 +0200

    faut optimiser les rotations

--- push_swap/src/push_swap.c
+++ push_swap/src/push_swap.c
@@ -6,35 +6,12 @@
 /*   By: zoum <zoum@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/11 17:38:07 by mzimeris          #+#    #+#             */
-/*   Updated: 2025/07/01 22:18:28 by zoum             ###   ########.fr       */
+/*   Updated: 2025/07/01 23:09:51 by zoum             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// make && clear && valgrind --track-origins=yes ./push_swap 1 0 9 5 3 4 6 7 2 8 > test.txt 
-
-// begin when the last split has been done
-// void final_merge(t_swap *swap)
-// {
-// 	ft_printf("before final rotate\n");
-// 	debug_print_stacks(swap);
-// 	ft_printf("min %d, max %d\n", swap->min, swap->max);
-// 	ft_printf("a-min %d, a-max %d\n", swap->stack_a->min, swap->stack_a->max);
-// 	ft_printf("b-min %d, b-max %d\n", swap->stack_b->min, swap->stack_b->max);
-// 	rotate_to(swap, find_max_in_split(swap->stack_a->first, swap->stack_a->len));
-// 	ft_push(swap, swap->stack_b->first);
-// 	while (swap->stack_b->len > 0)
-// 	{
-// 		rotate_to(swap, find_index(swap->stack_b, swap->stack_a->first->index - 1));
-// 		ft_push(swap, swap->stack_b->first);
-// 		ft_printf("len b %d\n", swap->stack_b->len);
-// 	debug_print_stacks(swap);
-// 	}
-// 	ft_printf("bouh\n");
-// 	rotate_to(swap, find_index(swap->stack_a, swap->stack_a->min));
-// 	debug_print_stacks(swap);
-// }
 void	print_moves(t_list *move)
 {
 	t_list	*current;
@@ -49,7 +26,6 @@ void	print_moves(t_list *move)
 		total_commands++;
 		current = current->next;
 	}
-	// ft_printf("count : %d\n", total_commands);
 }
 
 int	calculate_chunk_count(size_t len)
@@ -66,10 +42,9 @@ void	sort(t_swap *swap)
 {
 	while (swap->stack_a->len > 2)
 		ft_push(swap, swap->stack_a->first);
-	// ft_printf("empty a\n");
-	// debug_print_stacks(swap);
 	push_back_to_a_optimized(swap);
 }
+
 int	main(int argc, char *argv[])
 {
 	size_t		len;
@@ -82,14 +57,13 @@ int	main(int argc, char *argv[])
 	swap = swap_init(swap, argv, len);
 	push_chunks(swap, calculate_chunk_count(len));
 	sort(swap);
-	
 	rotate_to(swap, find_index(swap->stack_a, swap->stack_a->min));
 	debug_print_stacks(swap);
-	ft_printf("end sort\n");
 	print_moves(swap->move);
 	free_all(swap);
 	return (0);
 }
+
 // stocker les instructions avant de les imprimer pour faire du tri
 // supprimer les ra et rra qui se suivent
 // remplacer les ra + rb ou rra +rrb par rr / rrr
@@ -105,4 +79,3 @@ int	main(int argc, char *argv[])
 	// 		&& swap->stack_b->length != 0)
 		// push_swap(swap);
 	// ft_printf("sorted %d\n", is_circularly_sorted(swap->stack_a));
-	

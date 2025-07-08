017d478aa6cb2dea4156b38953ac04d191c664cd
mzimeris <mzimeris@student.42.fr>
2025-07-08 15:58:33 +0200

    cleaning op

--- push_swap/src/push_swap_r.c
+++ push_swap/src/push_swap_r.c
@@ -6,18 +6,23 @@
 /*   By: mzimeris <mzimeris@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 16:42:18 by zoum              #+#    #+#             */
-/*   Updated: 2025/07/08 15:51:18 by mzimeris         ###   ########.fr       */
+/*   Updated: 2025/07/08 15:57:51 by mzimeris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	ft_rotate(t_swap *swap, t_swap_int *elem)
+void	ft_rotate_a_b(t_swap *swap, t_swap_int *elem)
 {
 	if (!swap || !elem || elem->stack->len < 2)
 		return ;
 	elem->stack->first = elem->stack->first->next;
 	elem->stack->last = elem->stack->last->next;
+
+}
+
+void	ft_rotate(t_swap *swap, t_swap_int *elem)
+{
 	if (elem->stack == swap->stack_a)
 		ft_lstadd_back(&swap->move, ft_lstnew(ft_strdup("ra\n")));
 	else
@@ -28,6 +33,8 @@ void	ft_rotate_rr(t_swap *swap)
 {
 	if (!swap)
 		return ;
-	ft_rotate(swap, swap->stack_a->first);
-	ft_rotate(swap, swap->stack_b->first);
+	ft_rotate_a_b(swap, swap->stack_a->first);
+	ft_rotate_a_b(swap, swap->stack_b->first);
+	ft_lstadd_back(&swap->move, ft_lstnew(ft_strdup("rr\n")));
+
 }
